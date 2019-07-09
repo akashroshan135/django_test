7d9a7a5ef3dbc830cf75209f1511b51cbd610dae
Akash Roshan <akash.second1999@gmail.com>
2019-07-09 09:33:43 +0530

    added dummy data and passed it to the templates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author':'Man 1',
+        'title':'Blog post 1',
+        'content':'Content for blog post 1',
+        'date_posted':'08 July, 2019'
+    },
+    {
+        'author':'Man 1',
+        'title':'Blog post 2',
+        'content':'Content for blog post 2',
+        'date_posted':'08 July, 2019'
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', context={'title':'About'})
